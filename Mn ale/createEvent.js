6620e94a9c3b5351d7a08f36abe4ea8129cd59ae
Bereket Sintayehu <86576073+yonaSisay@users.noreply.github.com>
2022-02-23 08:38:31 +0000

    commiting create event and the card style

---
+++ Mn ale/createEvent.js
@@ -0,0 +1,13 @@
+var EventName = document.getElementById("Eventname").value;
+var Organizer = document.getElementById("Organizer").value;
+var typeEvent = document.getElementById("typeEvent").value;
+var Categories = document.getElementById("Categories").value;
+var comment = document.getElementById("comment").value;
+var location = document.getElementById("location").value;
+var date = document.getElementById("date").value;
+var imageFile = document.getElementById("file-selector").value;
+abe = "kebede";
+console.log(abe);
+document.getElementById("submit").addEventListener("click", ()=>{
+
+})
